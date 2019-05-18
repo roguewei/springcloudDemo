bec737b11ccc163e5d5309b1f7e10ff33e03cb4a
roguewei <1104837637@qq.com>
2019-05-18 17:16:37 +0800

    提交修改

--- microservicecloud-provider-dept-8001/src/main/java/com/winston/springcloud/DeptApplication.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/winston/springcloud/DeptApplication.java
@@ -2,6 +2,7 @@ package com.winston.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Winston
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/5/17 16:41
  */
 @SpringBootApplication
+// 本服务启动后自动注册进入eureka服务中
+@EnableEurekaClient
 public class DeptApplication {
 
     public static void main(String[] args) {
